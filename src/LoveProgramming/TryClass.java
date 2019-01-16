b2db51b59f93dcfa0c2e86ff5fd5660b0b0cdc7b
unknown <skoripalli@gmail.com>
2019-01-16 00:30:06 +0000

    frist commit

---
+++ src/LoveProgramming/TryClass.java
@@ -0,0 +1,13 @@
+package LoveProgramming;
+
+public class TryClass {
+    public static void main(String[] args) {
+        int sum =100+200;
+        String str = "We love + Java";
+        int incre= 10; incre = ++ incre;
+        System.out.println(sum);
+        System.out.println(str);
+        System.out.println(incre);
+
+    }
+}
